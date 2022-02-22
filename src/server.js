39af284bf3e868453f93efecbb95baf4d557e0e8
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-02-22 17:24:02 +0000

    updating to use cors

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ require("./db/connection");
 const express = require("express");
 const app = express();
 const userRouter = require("./user/userRoutes");
+const cors = require("cors");
 const port = 5001;
 
 app.use(express.json()); //allows API to parse JSON
+app.use(cors());
 app.use(userRouter);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
