842a9b4c1ae095d8375784705cc2faab62547163
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-02-28 11:25:02 +0000

    commiting older edits

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require("./db/connection");
 const express = require("express");
+const movieRouter = require("./movies/movieRoutes");
 const app = express();
 const userRouter = require("./user/userRoutes");
 const port = 5001;
 
 app.use(express.json()); //allows API to parse JSON
 app.use(userRouter);
+app.use(movieRouter);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
